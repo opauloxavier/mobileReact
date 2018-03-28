1353e85729730fba064bb2e7116d6ba4ba83b2b9
Paulo Roberto Xavier <contato.pauloxavier@gmail.com>
2018-03-28 19:25:15 -0300

    Adicionando componentes de outro projeto

---
+++ auth/src/components/common/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Header({ title }) {
+        const { textStyle, viewStyle } = styles;
+
+        return (
+            <View style={viewStyle}>
+                <Text style={textStyle}>{title}</Text>
+            </View>);
+}
+
+const styles = {
+    textStyle: {
+        fontSize: 20
+    },
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.6,
+        elevation: 2,
+        position: 'relative'
+    }
+};
