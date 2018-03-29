717be610ee919598efe312645b419576fe577a4d
Paulo Roberto Xavier <contato.pauloxavier@gmail.com>
2018-03-29 01:22:25 -0300

    ajustes na home

--- auth/App.js
+++ auth/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Header } from './src/components/common';
+import { View, Text } from 'react-native';
+import { Header, CardSection, Card, CustomButton } from './src/components/common';
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
         <Header title={'Auth'} />
+        <CardSection>
+          <Card>
+            <Text>
+              Login
+            </Text>
+          </Card>
+          <Card>
+            <Text>
+              Password
+            </Text>
+          </Card>
+        </CardSection>
+        <CardSection>
+          <Card>
+            <CustomButton onPress={() => console.log('CLICK')}>
+              Clique Aqui
+            </CustomButton>
+          </Card>
+        </CardSection>
       </View>
     );
   }
