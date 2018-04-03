ff46cd4cdda952c36fe74d849f6ab40933485e5a
Paulo Roberto Xavier <contato.pauloxavier@gmail.com>
2018-04-03 13:08:59 -0300

    Criando action e reducer de lista

--- techstack/App.js
+++ techstack/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Platform, UIManager } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { Header } from './src/components/common';
 import reducers from './src/reducers';
 import LibraryList from './src/components/LibraryList';
 
-export default class App extends React.Component {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    if (Platform.OS === 'android') {
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+}
   render() {
     return (
       <Provider store={createStore(reducers)}>
--- techstack/src/components/ListItem.js
+++ techstack/src/components/ListItem.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, LayoutAnimation, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
-export default class ListItem extends Component {
+class ListItem extends Component {
+
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+    
+    renderDescription() {
+        const { library, expanded } = this.props;
+
+        if (expanded) {
+            return (
+                <CardSection>
+                    <Text style={styles.textStyle}>
+                        {library.description}
+                    </Text>
+                </CardSection>
+            );
+        }
+    }
+
     render() {
         const { titleStyle } = styles;
+        const { title, id } = this.props.library;
 
         return (
-            <CardSection>
-                <Text style={titleStyle}> 
-                    {this.props.library.title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}> 
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -20,5 +49,16 @@ const styles = {
     titleStyle: {
         fontSize: 18,
         paddingLeft: 15
+    },
+    textStyle: {
+        flex: 1
     }
 };
+
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+
+    return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
