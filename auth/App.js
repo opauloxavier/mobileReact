d323e69ddbd17057e5dc7a6cfb2bd0303b4ec0cf
Vitor Machado <vitor.p.mach@gmail.com>
2021-05-21 15:54:01 -0300

    Atualiza readme e formatação

--- auth/App.js
+++ auth/App.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
-import { View } from 'react-native';
-import { Header, Spinner } from './src/components/common';
-import LoginForm from './src/components/LoginForm';
-import Logged from './src/components/Logged';
+import React, { Component } from "react";
+import firebase from "firebase";
+import { View } from "react-native";
+import { Header, Spinner } from "./src/components/common";
+import LoginForm from "./src/components/LoginForm";
+import Logged from "./src/components/Logged";
 
 export default class App extends Component {
-
   state = { loggedIn: null };
 
   componentDidMount() {
-      firebase.initializeApp({
-          apiKey: 'AIzaSyDhHB3RDsugdvDd4ZUgAWCcwADZKErcAvQ',
-          authDomain: 'authentication-1d12f.firebaseapp.com',
-          databaseURL: 'https://authentication-1d12f.firebaseio.com',
-          projectId: 'authentication-1d12f',
-          storageBucket: 'authentication-1d12f.appspot.com',
-          messagingSenderId: '854108323856'
-      });
+    firebase.initializeApp({
+      apiKey: "AIzaSyDhHB3RDsugdvDd4ZUgAWCcwADZKErcAvQ",
+      authDomain: "authentication-1d12f.firebaseapp.com",
+      databaseURL: "https://authentication-1d12f.firebaseio.com",
+      projectId: "authentication-1d12f",
+      storageBucket: "authentication-1d12f.appspot.com",
+      messagingSenderId: "854108323856",
+    });
 
-      firebase.auth().onAuthStateChanged((user) => {
-          if (user) {
-            this.setState({ loggedIn: true });
-          } else {
-            this.setState({ loggedIn: false });
-          }
-      });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 
   renderLogin = () => {
@@ -35,17 +34,16 @@ export default class App extends Component {
       case false:
         return <LoginForm />;
       default:
-        return <Spinner size='large' />;
+        return <Spinner size="large" />;
     }
   };
 
   render() {
     return (
       <View>
-        <Header title={'Authentication'} />
-          {this.renderLogin()}   
+        <Header title={"Authentication"} />
+        {this.renderLogin()}
       </View>
     );
   }
 }
-
